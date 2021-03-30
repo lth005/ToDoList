4a19978eb1440fa074268a0763b33c9c173ddd50
gloriapadilla <65581033+gloriapadilla@users.noreply.github.com>
2021-03-30 00:02:29 -0600

    BackEnd

---
+++ BackEnd/server/controller.js
@@ -0,0 +1,293 @@
+//Importamos las librerias que serán necesarias
+const express = require('express');
+const router = express.Router();
+const http = require('http');
+var mysql = require('mysql');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret_key = process.env.SECRET_KEY || "prew";
+
+require('dotenv').config();
+const servidor = 'localhost';
+const usuario = 'root';
+const clave = 'emma63194';
+const baseDatos = 'todolist';
+
+//Creamos la conexión a la base de datos¨
+console.log(process.env.HOST);
+
+var con = mysql.createPool({
+    host: servidor,
+    user: usuario,
+    password: clave,
+    database: baseDatos,
+    insecureAuth: true,
+    multipleStatements: true
+});
+
+//
+router.get('/get_tareas', (req, res, next) => {
+    var query = 'select * from tareas where id_estado_tarea = 1';
+    con.query(query, (err, result, field) => {
+        if (err){
+            next(err);
+        } else {
+            res.status(200).json(result)
+        }
+    });
+});
+
+router.post('/insert_tarea', (req, res, next) => {
+    var query = 'insert into tareas (titulo, descripcion, id_estado_tarea) values (?,?,?);';
+    var values = [
+                req.body.titulo,
+                req.body.descripcion,
+                req.body.id_estado_tarea];
+    con.query(query, values, (err, result, field) => {
+        if(err){
+            next(err);
+        } else {
+            res.status(200).json(result)
+        }
+    });            
+});
+
+router.put('/update_tarea', (req, res, next) => {
+    var query = 'update tareas set titulo=?, descripcion=? where id_tarea=?';
+    var values = [req.body.titulo,
+                req.body.descripcion,
+                req.body.id_tarea];
+    con.query(query, values, (err, result, field) => {
+        if(err){
+            next(err);
+        } else {
+            res.status(200).json(result)
+        }
+    });            
+});
+
+router.put('/delete_tarea', (req, res, next) => {
+    var query = 'update tareas set id_estado_tarea=? where id_tarea=?';
+    var values = [req.body.id_estado_tarea,
+                req.body.id_tarea];
+    con.query(query, values, (err, result, field) => {
+        if(err){
+            next(err);
+        } else {
+            res.status(200).json(result)
+        }
+    });            
+});
+
+router.get('/get_usuarios', (req, res, next) => {
+    var query = 'select * from usuarios where id_estado = 1';
+    con.query(query, (err, result, field) => {
+        if (err){
+            next(err);
+        } else {
+            res.status(200).json(result)
+        }
+    });
+});
+
+router.post('/insert_usuario', (req, res, next) => {
+    var values = [
+        req.body.nombre_usuario,
+        req.body.correo_electronico,
+        req.body.contraseña,
+        req.body.id_rol,
+        req.body.id_estado
+    ];
+
+    var query = 'insert into usuarios (nombre_usuario, correo_electronico, contraseña, id_rol, id_estado) values (?,?,md5(?),?,?);';
+        con.query(query, values, (err, result, fields) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send();
+            } else {
+                res.status(200).send();
+            }
+        });
+});
+
+router.put('/update_contrasenia', (req, res, next) => {
+    var contra = [
+        {contrasenia: req.body.contraseña},
+        {id_usuario: req.body.id_usuario}
+    ];
+    var values = [req.body.contraseña,
+                req.body.id_usuario];
+
+    var contrasenia = req.body.contraseña;
+    var id = parseInt( req.body.id_usuario);
+
+    var query = 'update usuarios set contraseña=md5(?) where id_usuario=?;';
+        console.log(id + " " + contrasenia);
+        con.query(query, values, (err, result, fields) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send();
+            } else {
+                res.status(200).send();
+            }
+            console.log(result + " " + query);
+        });
+});
+
+router.post('/login', (req, res, next) => {
+    var values = [req.body.nombre_usuario, req.body.contraseña];
+
+    var query = "select * from usuarios where nombre_usuario = ? and contraseña = md5(?)";
+    con.query(query, values, (err, result, fields) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send();
+        } else {
+            res.status(200).json(result);
+            if(result.length > 0){
+                console.log("El usuario " + req.body.nombre_usuario + " ha iniciado sesion.");
+            } else {
+                console.log("Error como la vida de Edwin.");
+            }
+        }
+    });
+})
+
+/*//APIs CRUD para mantenimiento de Vehiculos
+router.get('/get_vehiculos', (req, res, next) => {
+    var query = 'select * from vehiculos';
+    con.query(query, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.get('/get_vehiculo', (req, res, next) => {
+    var query = 'select * from vehiculos where placa = ?';
+    var values = [req.query.placa];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.post('/insert_vehiculo', (req, res, next) => {
+    var query = 'INSERT INTO Vehiculos (placa, color, marca, modelo) values (?, ?, ?, ?)';
+    var values = [req.body.placa,
+                  req.body.color,
+                  req.body.marca,
+                  req.body.modelo];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.put('/update_vehiculo', (req, res, next) => {
+    var query = 'Update Vehiculos set color=?, marca=?, modelo=? WHERE placa = ?';
+    
+    var values = [req.body.color,
+                  req.body.marca,
+                  req.body.modelo,
+                  req.body.placa];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.delete('/delete_vehiculo', (req, res, next) => {
+    var query = 'delete from vehiculos.vehiculos where placa = ?';
+    
+    var values = [req.query.placa];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+//APIs para Manejo de usuarios
+
+router.get('/get_usuarios', (req, res, next) => {
+    var query = 'select * from usuarios';
+    con.query(query, (err, result, fields) => {
+        if(err) {
+            next(err);
+        } else {
+            res.status(200).json(result);
+        }
+    });
+});
+
+router.post('/insert_usuario', (req, res, next) => {
+    var user = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    const create_user = (user) => {
+        var query = "INSERT INTO usuarios (username, password) VALUES (?) ";
+        con.query(query, [Object.values(user)], (err, result, fields) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send();
+            } else {
+                res.status(200).send();
+            }
+        });
+    };
+    bcrypt.hash(user.password, 10).then((hashedPassword) => {
+        user.password = hashedPassword;
+        create_user(user);
+    });
+});
+
+router.post('/login', (req,res,next) =>{
+    var user = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    const get_token = (user) => {
+        var query = "SELECT USERNAME, PASSWORD FROM usuarios WHERE username = ?"
+        con.query(query, [user.username], (err, result, fields) => {
+            if (err || result.length == 0) {
+                console.log(err);
+                res.status(400).json({message:"Usuario o Contraseña Incorrectos"});
+            } else {
+                bcrypt.compare(user.password,result[0].PASSWORD, (error, isMatch)=> {
+                    if (isMatch){
+                        var token = jwt.sign({userId: result[0].id}, secret_key);
+                        res.status(200).json({token});
+                    }else if (error){
+                        res.status(400).json(error);
+                    }else {
+                        res.status(400).json({message: "Usuario o Contraseña Incorrectos"});
+                    }
+                });
+            }
+        });
+    }
+    get_token(user);
+
+});*/
+
+
+module.exports = router;
---
+++ todolist.sql
@@ -0,0 +1,327 @@
+use todolist;
+-- Dump completed on 2021-03-28 21:17:02
+
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `id_rol` int NOT NULL,
+  `nombre_rol` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 21:17:04
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estado_tareas`
+--
+
+DROP TABLE IF EXISTS `estado_tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estado_tareas` (
+  `id_estado_tarea` int NOT NULL,
+  `nombre_estado_tarea` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_estado_tarea`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estado_tareas`
+--
+
+LOCK TABLES `estado_tareas` WRITE;
+/*!40000 ALTER TABLE `estado_tareas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estado_tareas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 21:17:03
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `estados`
+--
+
+DROP TABLE IF EXISTS `estados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estados` (
+  `idestado` int NOT NULL,
+  `nombre_estado` varchar(100) NOT NULL,
+  PRIMARY KEY (`idestado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estados`
+--
+
+LOCK TABLES `estados` WRITE;
+/*!40000 ALTER TABLE `estados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(100) NOT NULL,
+  `correo_electronico` varchar(100) NOT NULL,
+  `contraseña` varchar(512) NOT NULL,
+  `id_rol` int NOT NULL,
+  `id_estado` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `fkidrol_idx` (`id_rol`),
+  KEY `FKestado_idx` (`id_estado`),
+  CONSTRAINT `FKestado` FOREIGN KEY (`id_estado`) REFERENCES `estados` (`idestado`),
+  CONSTRAINT `FKidrol` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 21:17:03
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `listados`
+--
+
+DROP TABLE IF EXISTS `listados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `listados` (
+  `id_usuario` int NOT NULL,
+  `id_tarea` int NOT NULL,
+  `fecha_realizado` date NOT NULL,
+  PRIMARY KEY (`id_usuario`,`id_tarea`),
+  KEY `idtarea_idx` (`id_tarea`),
+  CONSTRAINT `idtarea` FOREIGN KEY (`id_tarea`) REFERENCES `tareas` (`id_tarea`),
+  CONSTRAINT `idusuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listados`
+--
+
+LOCK TABLES `listados` WRITE;
+/*!40000 ALTER TABLE `listados` DISABLE KEYS */;
+/*!40000 ALTER TABLE `listados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+
+-- Dump completed on 2021-03-28 21:17:01
+
+
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todolist2021
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tareas`
+--
+
+DROP TABLE IF EXISTS `tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tareas` (
+  `id_tarea` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(80) NOT NULL,
+  `descripcion` varchar(4000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `id_estado_tarea` int NOT NULL,
+  PRIMARY KEY (`id_tarea`),
+  KEY `FKestadotarea_idx` (`id_estado_tarea`),
+  CONSTRAINT `FKestadotarea` FOREIGN KEY (`id_estado_tarea`) REFERENCES `estado_tareas` (`id_estado_tarea`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tareas`
+--
+
+LOCK TABLES `tareas` WRITE;
+/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-28 21:17:00
+
