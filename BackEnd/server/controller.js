9ed8eebd786950098eaca661936812b51d148be8
lth005 <65581061+lth005@users.noreply.github.com>
2021-04-04 20:38:15 -0600

    actualizar

--- BackEnd/server/controller.js
+++ BackEnd/server/controller.js
@@ -11,7 +11,7 @@ const secret_key = process.env.SECRET_KEY || "prew";
 require('dotenv').config();
 const servidor = 'localhost';
 const usuario = 'root';
-const clave = 'emma63194';
+const clave = 'cearacely00';
 const baseDatos = 'todolist';
 
 //Creamos la conexión a la base de datos¨
@@ -228,24 +228,34 @@ router.get('/get_usuarios', (req, res, next) => {
 });
 
 router.post('/insert_usuario', (req, res, next) => {
-    var values = [
-        req.body.nombre_usuario,
-        req.body.correo_electronico,
-        req.body.contrasenia,
-        req.body.id_rol,
-        req.body.id_estado
 
-    ];
-
-    var query = 'insert into usuarios (nombre_usuario, correo_electronico, contrasenia, id_rol, id_estado) values (?,?,md5(?),?,?);';
-    con.query(query, values, (err, result, fields) => {
+    var query1='SELECT * FROM usuarios WHERE correo_electronico=?';
+    var values1=[req.body.correo_electronico];
+    con.query(query1, values1, (err, result, fields) => {
         if (err) {
             console.log(err);
             res.status(500).send();
         } else {
-            res.status(200).send();
+            var values2 = [
+                req.body.nombre_usuario,
+                req.body.correo_electronico,
+                req.body.contrasenia,
+                req.body.id_rol,
+                req.body.id_estado
+        
+            ];
+            var query2 = 'insert into usuarios (nombre_usuario, correo_electronico, contrasenia, id_rol, id_estado) values (?,?,md5(?),?,?);';
+            con.query(query2, values2, (err, result, fields) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send();
+                } else {
+                    res.status(200).send();
+                }
+            });
         }
     });
+   
 });
 
 router.put('/update_contrasenia', (req, res, next) => {
@@ -338,141 +348,6 @@ router.post('/insert_listado', (req, res, next) => {
     });
 });
 
-/*//APIs CRUD para mantenimiento de Vehiculos
-router.get('/get_vehiculos', (req, res, next) => {
-    var query = 'select * from vehiculos';
-    con.query(query, (err, result, field) => {
-       if(err){
-           next(err);
-       } else {
-           res.status(200).json(result)
-       }
-      });
-});
-
-router.get('/get_vehiculo', (req, res, next) => {
-    var query = 'select * from vehiculos where placa = ?';
-    var values = [req.query.placa];
-
-    con.query(query, values, (err, result, field) => {
-       if(err){
-           next(err);
-       } else {
-           res.status(200).json(result)
-       }
-      });
-});
-
-router.post('/insert_vehiculo', (req, res, next) => {
-    var query = 'INSERT INTO Vehiculos (placa, color, marca, modelo) values (?, ?, ?, ?)';
-    var values = [req.body.placa,
-                  req.body.color,
-                  req.body.marca,
-                  req.body.modelo];
-
-    con.query(query, values, (err, result, field) => {
-       if(err){
-           next(err);
-       } else {
-           res.status(200).json(result)
-       }
-      });
-});
-
-router.put('/update_vehiculo', (req, res, next) => {
-    var query = 'Update Vehiculos set color=?, marca=?, modelo=? WHERE placa = ?';
-    
-    var values = [req.body.color,
-                  req.body.marca,
-                  req.body.modelo,
-                  req.body.placa];
-
-    con.query(query, values, (err, result, field) => {
-       if(err){
-           next(err);
-       } else {
-           res.status(200).json(result)
-       }
-      });
-});
-
-router.delete('/delete_vehiculo', (req, res, next) => {
-    var query = 'delete from vehiculos.vehiculos where placa = ?';
-    
-    var values = [req.query.placa];
-
-    con.query(query, values, (err, result, field) => {
-       if(err){
-           next(err);
-       } else {
-           res.status(200).json(result)
-       }
-      });
-});
-
-//APIs para Manejo de usuarios
-
-router.get('/get_usuarios', (req, res, next) => {
-    var query = 'select * from usuarios';
-    con.query(query, (err, result, fields) => {
-        if(err) {
-            next(err);
-        } else {
-            res.status(200).json(result);
-        }
-    });
-});
-
-router.post('/insert_usuario', (req, res, next) => {
-    var user = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    const create_user = (user) => {
-        var query = "INSERT INTO usuarios (username, password) VALUES (?) ";
-        con.query(query, [Object.values(user)], (err, result, fields) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send();
-            } else {
-                res.status(200).send();
-            }
-        });
-    };
-    bcrypt.hash(user.password, 10).then((hashedPassword) => {
-        user.password = hashedPassword;
-        create_user(user);
-    });
-});
-
-router.post('/login', (req,res,next) =>{
-    var user = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    const get_token = (user) => {
-        var query = "SELECT USERNAME, PASSWORD FROM usuarios WHERE username = ?"
-        con.query(query, [user.username], (err, result, fields) => {
-            if (err || result.length == 0) {
-                console.log(err);
-                res.status(400).json({message:"Usuario o contrasenia Incorrectos"});
-            } else {
-                bcrypt.compare(user.password,result[0].PASSWORD, (error, isMatch)=> {
-                    if (isMatch){
-                        var token = jwt.sign({userId: result[0].id}, secret_key);
-                        res.status(200).json({token});
-                    }else if (error){
-                        res.status(400).json(error);
-                    }else {
-                        res.status(400).json({message: "Usuario o contrasenia Incorrectos"});
-                    }
-                });
-            }
-        });
-    }
-    get_token(user);
-
-});*/
 
 
 module.exports = router;
